535898658b09ee6a1b975cf3023067a4193d4cff
chaitapatel <chaitapatel2789@gmail.com>
2026-09-07 11:18:31 +0530

    Add files via upload

---
+++ project_4output.py
@@ -0,0 +1,129 @@
+#welcome to the data analyzer and transformer program
+
+#1. Input Data
+#2. Display Data Summary
+#3. Calculate Factorial
+#5. Sort Data
+#6. Display Statistics
+#7. Exit
+#8. User Defined Functions
+#9. Display 2D Data
+#10. Transform Data
+#11. *args
+#12. **kwargs
+#13. Fibonacci
+#Enter your choice: 1
+#1. 1D List
+#2. 2D List
+#3. Sample Data
+#Enter your choice: 1
+#Enter values: 34 12 56 78 43 21 90
+#Data stored successfully!
+
+#1. Input Data
+#2. Display Data Summary
+#3. Calculate Factorial
+#4. Filter Data
+#5. Sort Data
+#6. Display Statistics
+#7. Exit
+#8. User Defined Functions
+#9. Display 2D Data
+#10. Transform Data
+#11. *args
+#12. **kwargs
+#13. Fibonacci
+#Enter your choice: 2
+#Total elements: 7
+#Minimum value: 12
+#Maximum value: 90
+#Sum: 334
+#Average: 47.71
+
+#1. Input Data
+#2. Display Data Summary
+#3. Calculate Factorial
+#4. Filter Data
+#5. Sort Data
+#6. Display Statistics
+#7. Exit
+#8. User Defined Functions
+#9. Display 2D Data
+#10. Transform Data
+#11. *args
+#12. **kwargs
+#13. Fibonacci
+#Enter your choice: 3
+#Enter number: 5
+#Factorial: 120
+
+#1. Input Data
+#2. Display Data Summary
+#3. Calculate Factorial
+#4. Filter Data
+#5. Sort Data
+#6. Display Statistics
+#7. Exit
+#8. User Defined Functions
+#9. Display 2D Data
+#10. Transform Data
+#11. *args
+#12. **kwargs
+#13. Fibonacci
+#Enter your choice: 4
+#Enter threshold: 50
+#Filtered Data: [56, 78, 90]
+
+#1. Input Data
+#2. Display Data Summary
+#3. Calculate Factorial
+#4. Filter Data
+#5. Sort Data
+#6. Display Statistics
+#7. Exit
+#8. User Defined Functions
+#9. Display 2D Data
+#10. Transform Data
+#11. *args
+#12. **kwargs
+#13. Fibonacci
+#Enter your choice: 5
+#1. Ascending
+#2. Descending
+#Enter choice: 1
+#Sorted Data: [12, 21, 34, 43, 56, 78, 90]
+
+#1. Input Data
+#2. Display Data Summary
+#3. Calculate Factorial
+#4. Filter Data
+#5. Sort Data
+#6. Display Statistics
+#7. Exit
+#8. User Defined Functions
+#9. Display 2D Data
+#10. Transform Data
+#11. *args
+#12. **kwargs
+#13. Fibonacci
+#Enter your choice: 6
+#Minimum: 12
+#Maximum: 90
+#Sum: 334
+#Average: 47.71
+
+#1. Input Data
+#2. Display Data Summary
+#3. Calculate Factorial
+#4. Filter Data
+#5. Sort Data
+#6. Display Statistics
+#7. Exit
+#8. User Defined Functions
+#9. Display 2D Data
+#10. Transform Data
+#11. *args
+#12. **kwargs
+#13. Fibonacci
+#Enter your choice: 7
+ #thank you for using the data analyzer and transformer program. Goodbye!
